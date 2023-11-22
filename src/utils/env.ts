6170f44b923cf3bedf8d6a6b9e18eedc11354972
lixin <13798516431@126.com>
2023-11-22 14:40:00 +0800

    添加global变量处理

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,5 +3,20 @@ export const getEnv = function (key:string) {
     return value || "";
 }
 
+const getKey = function (key:string) {
+    const value = import.meta.env[key];
+    return value || "";
+}
+
+const getSystemService = function(gateway:string,serviceName:string){
+    const prefix = getKey(gateway);
+    const serviceNamePrefix = getKey(serviceName);
+    return prefix + serviceNamePrefix;
+}
+
 export const GATEWAY_BASE_URL = getEnv("VITE_GATEWAY_API_PREFIX_PATH");
+
+export const SYSTEM_SERVICE = getSystemService("VITE_GATEWAY_API_PREFIX_PATH","VITE_SYSTEM_SERVICE");
+
+export const STARLINK_SERVICE = getSystemService("VITE_GATEWAY_API_PREFIX_PATH","VITE_STARLINK_SERVICE");
 
