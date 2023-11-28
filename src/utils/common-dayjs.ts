1d465e29555e9cb740521cc8e0c003989ee4e517
lixin <13798516431@126.com>
2023-11-28 20:07:49 +0800

    完善打包

--- src/utils/common-dayjs.ts
+++ src/utils/common-dayjs.ts
@@ -4,7 +4,7 @@
 import * as dayjs from 'dayjs'
 import * as isLeapYear from 'dayjs/plugin/isLeapYear' // 导入插件
 import 'dayjs/locale/zh-cn' // 导入本地化语言
-dayjs.extend(isLeapYear) // 使用插件
+// dayjs.extend(isLeapYear) // 使用插件
 dayjs.locale('zh-cn')
 
 
