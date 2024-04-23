757c9a3c7b4ef0581ccd6f35cf14c4143d984930
lixin <13798516431@126.com>
2024-04-23 13:43:37 +0800

    完善bpmn流程

---
+++ src/utils/pipeline-event-handler.ts
@@ -0,0 +1,38 @@
+// @ts-nocheck  
+// ts不检查该文件,否则,打包都不能通过
+
+import localStore from '@hooshid/local-store'
+import { emitter } from '@/utils/mitt'
+
+
+export function registerPipelineEventHandler(){
+    // 1. pipeline-active-node
+    emitter.on('pipeline-active-node', (data) => {
+        if (data?.body) {
+            const bodyObject = JSON.parse(data.body)
+            const businessId = bodyObject?.businessId
+            const nodeId = bodyObject?.nodeId
+            if(businessId && nodeId){
+                console.log("&&&&&&&&&&&&&&&&&store&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+                console.log(businessId + "-->" + nodeId)
+                console.log("&&&&&&&&&&&&&&&&&store&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+                localStore.set(businessId , nodeId , "1h")
+            }
+        }
+    });
+
+    // 2. pipeline-complete
+    emitter.on("pipeline-complete",(data)=>{
+        if(data?.body){
+			const bodyObject = JSON.parse(data.body)
+			const businessId = bodyObject?.businessId
+            if(businessId){
+                localStore.remove(businessId)
+            }
+        }
+    })
+}
+
+export function getActiveNode(businessId:string){
+    return localStore.get(businessId)
+}
